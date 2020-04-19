1452a92386e6a743abf033c7cc88b583160a6d85
Dmitrii Pimonov <dapimonov@gmail.com>
2020-04-19 22:23:50 +0300

    Updates

---
+++ precache-manifest.113af4d117078ff8f9d1bff5ca60cb3d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "972e450a5478fb8400d6ba1d45ecfb19",
+    "url": "/test_assignments-aviasales_frontend/index.html"
+  },
+  {
+    "revision": "04bda51f35d235d3f611",
+    "url": "/test_assignments-aviasales_frontend/static/css/main.a2a000e2.chunk.css"
+  },
+  {
+    "revision": "f0a91f5e29dfcc0f0581",
+    "url": "/test_assignments-aviasales_frontend/static/js/2.9398603e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test_assignments-aviasales_frontend/static/js/2.9398603e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04bda51f35d235d3f611",
+    "url": "/test_assignments-aviasales_frontend/static/js/main.c574eb70.chunk.js"
+  },
+  {
+    "revision": "162a96325a5c22ea5b6c",
+    "url": "/test_assignments-aviasales_frontend/static/js/runtime-main.d70fec08.js"
+  },
+  {
+    "revision": "68ca57dc522eda771a31bb07bdd256b1",
+    "url": "/test_assignments-aviasales_frontend/static/media/check.68ca57dc.svg"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/test_assignments-aviasales_frontend/static/media/logo.0c089234.svg"
+  },
+  {
+    "revision": "811c8baeca3bb3329488f729979c1fbc",
+    "url": "/test_assignments-aviasales_frontend/static/media/opensans.811c8bae.woff"
+  }
+]);
